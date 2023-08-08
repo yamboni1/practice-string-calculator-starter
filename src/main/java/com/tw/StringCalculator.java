b66411ca7b2a9e3a9805b356d4d03fb7f0379f09
NICOLE YAMBOT (OPS-IRIS-ISD-OOCLL/MNL) <YAMBONI@OOCL.COM>
2023-08-08 16:56:08 +0800

    Created string calculator

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,22 @@
 package com.tw;
 
 public class StringCalculator {
-    public int add(String string) {
-        return 0;
+       public int add(String string) {
+        int sum = 0;
+        String[] splitInt = string.split("");
+        int[] numbers = new int[splitInt.length]; // gets the length of the string array
+
+        for (int i = 0; i < splitInt.length; i++){
+           
+            numbers[i] = Integer.parseInt(splitInt[i]);
+        }
+        for(int j = 0; j < numbers.length; j++)
+        {
+            System.out.println(numbers[j]);
+            sum = sum + numbers[j];
+
+        }
+        return sum;
+        
     }
 }
